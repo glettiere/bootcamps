0db4c751a7cb060a64c5f460de5bc13b89f0cd50
glettiere <g.lettiere@gmail.com>
2021-08-18 06:15:02 -0300

    Rev1

---
+++ intro JavaScript 101/js/main.js
@@ -0,0 +1,21 @@
+var nome ="Gilbert Lettiere";
+var idade = 42
+var idade2 = 10
+var frase = "Tecnologia é o melhor tipo de noticía para se consumir diáriamente"
+
+//alert(nome + " tem " + idade + " anos")
+//alert(idade + idade2)
+
+var lista =["maça","pera","laranja"]
+//alert(lista)
+
+console.log(nome)
+console.log (idade)
+console.log(idade2)
+console.log(idade+idade2)
+console.log(frase)
+console.log(frase.replace ("Tecnologia","Informática")) 
+//alert(frase.replace("Tecnologia","Informática"))
+console.log(frase.toUpperCase())
+console.log(frase.toLowerCase())
+console.log(lista)
